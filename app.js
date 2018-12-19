0a668d3a1dc3b5cfcc7e76f8d07dc1c6fa0aa5df
Phanit Pollavith <phanit.pollavith@chimpchange.me>
2018-12-18 17:26:14 -0800

    authentication

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var product = require('./routes/product'); // Imports routes for the products
 var user = require('./routes/user');  // Imports routes for the user
 var app = express();
 
+const path = require('path');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
 // Set up mongoose connection
 var mongoose = require('mongoose');
 let dev_db_url = 'mongodb://someuser:ABC1234@ds139124.mlab.com:39124/productstutorial';
@@ -14,18 +22,49 @@ let dev_db_url = 'mongodb://someuser:ABC1234@ds139124.mlab.com:39124/productstut
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
-app.use('/users', user);
+// app.use('/users', user);
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-// var port = 3100;
-//
-// app.listen(port, () => {
-//     console.log('Server is up and running on port numner ' + port);
-// });
+// Models and Config
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
 
 module.exports = {app, express}; // passing in objects {app, and express}, can import specific objects in other files
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-var user_controller = require('../controllers/user');
-
-router.get('/test', user_controller.test);
-router.post('/create', user_controller.user_create);
-
-module.exports = router;
+// var express = require('express');
+// var router = express.Router();
+//
+// var user_controller = require('../controllers/user');
+//
+// router.get('/test', user_controller.test);
+// router.post('/create', user_controller.user_create);
+//
+// module.exports = router;
